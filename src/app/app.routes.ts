4909d22878e6a471bc376698788bcb1790b03b83
PC . <hk92vnq3vp0@gmail.com>
2025-05-02 22:47:23 -0400

    feat: add medic and user register components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { UserRegisterComponent } from './pages/user-register/user-register.component';
+import { MedicRegisterComponent } from './pages/medic-register/medic-register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    
+    {
+        path: '',
+        redirectTo: 'UserRegister',
+        pathMatch: 'full',
+    },
+
+    {
+        path: 'UserRegister',
+        component: UserRegisterComponent,
+    },
+    {
+        path: 'MedicRegister',
+        component: MedicRegisterComponent,
+    },
+];
---
+++ src/app/pages/medic-register/medic-register.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medic-register',
+  standalone: true,
+  imports: [],
+  templateUrl: './medic-register.component.html',
+  styleUrl: './medic-register.component.css'
+})
+export class MedicRegisterComponent {
+
+}
